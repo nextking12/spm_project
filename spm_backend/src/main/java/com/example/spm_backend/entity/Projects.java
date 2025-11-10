3a48584b79800c3e6f7885c49a515bf7fc19de58
Edward King <king8three@gmail.com>
2025-11-10 15:30:57 -0500

    update cleaned up errors

--- spm_backend/src/main/java/com/example/spm_backend/entity/Projects.java
+++ spm_backend/src/main/java/com/example/spm_backend/entity/Projects.java
@@ -1,5 +1,9 @@
 package com.example.spm_backend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,7 +26,11 @@ public class Projects {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    
+    @Column(name = "name", nullable = false,unique = true)
+    @NotBlank(message = "Project name is required")
+    @Size(min = 1, max = 255, message ="Project name must be between 1 and 255 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9_\\-\\.\\s]+$", message = "Script name contains invalid characters")
+    private String name;
 
 
 
