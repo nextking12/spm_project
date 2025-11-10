15a44b226f1deae01a319fae469185dec773738e
Edward King <king8three@gmail.com>
2025-11-10 15:15:28 -0500

    started work on project entity

---
+++ spm_backend/src/main/java/com/example/spm_backend/entity/Projects.java
@@ -0,0 +1,29 @@
+package com.example.spm_backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="projects")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Projects {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+
+
+
+}
