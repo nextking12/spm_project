2d3a7434c6dcd380b37a76c734547702c113e45d
Edward King <king8three@gmail.com>
2025-12-31 12:50:37 -0500

    starting work on service

--- spm_project_back_java/src/main/java/com/example/spm_project_back_java/service/ProjectService.java
+++ spm_project_back_java/src/main/java/com/example/spm_project_back_java/service/ProjectService.java
@@ -2,7 +2,14 @@ package com.example.spm_project_back_java.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.spm_project_back_java.repository.ProjectRepository;
+
 @Service
 public class ProjectService {
+    
+    private final ProjectRepository projectRepository;
+    
+    
+
 
 }
